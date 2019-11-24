4641fc52f44a5d03bb4e0c8ebce695f2d36781c6
Lamiarezgui <rezguilamia1401@gmail.com>
2019-11-24 14:18:06 +0100

    change

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { ContactComponent } from './contact/contact.component';
+import { Error404Component } from './error404/error404.component';
+import { ConnecterComponent } from './connecter/connecter.component';
+
+const routes: Routes = [
+  {path:'accueil', component:AccueilComponent},
+  {path:'produits', component:ProduitsComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'connecter', component:ConnecterComponent},
+  {path:'', redirectTo:'accueil', pathMatch:'full'},
+  {path:'**', component:Error404Component}
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
